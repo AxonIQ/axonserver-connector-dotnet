9d4db9d4ffc80a2543facbb9df8dd783f1a8d7e4
yreynhout <yves.reynhout@bittacklr.be>
2021-11-30 10:39:43 +0100

    make note we need specific intergration tests wrt authentication

--- src/AxonIQ.AxonServer.Connector/NoServerAuthentication.cs
+++ src/AxonIQ.AxonServer.Connector/NoServerAuthentication.cs
@@ -6,5 +6,6 @@ internal class NoServerAuthentication : IAxonServerAuthentication
 {
     public void WriteTo(Metadata metadata)
     {
+        //TODO: Write an integration test that sends a request without an access token and one with an empty access token
     }
 }
