c5093783e1c58e41b302638e80d99b26322fa99e
yreynhout <yves.reynhout@bittacklr.be>
2023-11-21 14:40:47 +0100

    fix: purging events using context recreation

--- src/AxonIQ.AxonServer.Embedded/AxonServerExtensions.cs
+++ src/AxonIQ.AxonServer.Embedded/AxonServerExtensions.cs
@@ -1,10 +1,48 @@
+using AxonIQ.AxonServer.Connector;
+using Io.Axoniq.Axonserver.Grpc.Admin;
+
 namespace AxonIQ.AxonServer.Embedded;
 
 public static class AxonServerExtensions
 {
     public static async Task PurgeEventsAsync(this IAxonServer server)
     {
-        using var client = server.CreateHttpClient();
-        (await client.DeleteAsync("v1/devmode/purge-events")).EnsureSuccessStatusCode();
+        await using var connection = new AxonServerConnection(Context.Admin, AxonServerConnectorOptions
+            .For(ComponentName.Default)
+            .WithRoutingServers(server.GetGrpcEndpoint())
+            .Build());
+        await connection.WaitUntilConnectedAsync();
+        
+        //Delete the context
+        await connection.AdminChannel.DeleteContextAsync(new DeleteContextRequest
+        {
+            Name = Context.Default.ToString(),
+            PreserveEventStore = false
+        });
+        
+        //Wait for the context to be deleted
+        var deleted = false;
+        while (!deleted)
+        {
+            var contexts = await connection.AdminChannel.GetAllContextsAsync();
+            deleted = contexts.All(context => context.Name != Context.Default.ToString());
+        }
+        
+        //Recreate the context
+        await connection.AdminChannel.CreateContextAsync(new CreateContextRequest
+        {
+            Name = Context.Default.ToString(),
+            ReplicationGroupName = Context.Default.ToString()
+        });
+        
+        //Wait for the context to be created
+        var created = false;
+        while (!created)
+        {
+            var contexts = await connection.AdminChannel.GetAllContextsAsync();
+            created = contexts.Any(context => context.Name == Context.Default.ToString());
+        }
+
+        await Task.Delay(1000);
     }
 }
